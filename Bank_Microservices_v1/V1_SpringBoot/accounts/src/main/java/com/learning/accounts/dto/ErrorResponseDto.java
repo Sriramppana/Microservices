fd492dc31a5a65d11c43e0534b17157efa5b4f2d
Your Name <you@example.com>
2025-11-17 15:57:12 +0530

    Commit

---
+++ Bank_Microservices_v1/V1_SpringBoot/accounts/src/main/java/com/learning/accounts/dto/ErrorResponseDto.java
@@ -0,0 +1,25 @@
+package com.learning.accounts.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@Schema(
+        name = "ErrorResponse",
+        description = "Schema to hold error response information"
+)
+public class ErrorResponseDto {
+    @Schema(description = "API Path", example = "/api/v1/accounts")
+    private String apiPath;
+    @Schema(description = "Error Code", example = "400")
+    private HttpStatus errorCode;
+    @Schema(description = "Error Message", example = "Account not found")
+    private String errorMessage;
+    @Schema(description = "Error Time", example = "2022-01-01T00:00:00")
+    private LocalDateTime errorTime;
+}
