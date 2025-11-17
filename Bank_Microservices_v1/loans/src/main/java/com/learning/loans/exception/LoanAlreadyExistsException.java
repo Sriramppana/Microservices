a3dafdf08b0ebd3e554621a4a7a0f5163ad15c31
Your Name <you@example.com>
2025-11-17 15:59:04 +0530

    Rebase the folder

--- Bank_Microservices_v1/loans/src/main/java/com/learning/loans/exception/LoanAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.learning.loans.exception;
-
-public class LoanAlreadyExistsException extends RuntimeException {
-    public LoanAlreadyExistsException(String message) {
-        super(message);
-    }
-}
