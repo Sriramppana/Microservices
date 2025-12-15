46e7bcc2354cef1185584c52f69b5fead03a59f6
Your Name <you@example.com>
2025-12-15 15:25:06 +0530

    Initial commit of v3

---
+++ Bank_Microservices_v3(SR&SD)/accounts/src/main/java/com/learning/accounts/dto/AccountsDto.java
@@ -0,0 +1,26 @@
+package com.learning.accounts.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+
+@Data
+@Schema(
+        name="Accounts",
+        description="Schema to hold Accounts information"
+)
+public class AccountsDto {
+    @NotEmpty(message = "Account Number is required")
+    @Pattern(regexp = "$|[0-9]{10}",message="Mobile Number should be 10 digits value")
+   @Schema(description="Account Number of the customer",example="9234567890")
+    private Long accountNumber;
+
+    @NotEmpty(message = "Account type can not be a null or empty")
+    @Schema(description="Account type of the customer",example="Savings")
+    private String accountType;
+
+    @NotEmpty(message = "Account type can not be a null or empty")
+    @Schema(description="Branch Address of the customer",example="123 Main Street")
+    private String branchAddress;
+}
