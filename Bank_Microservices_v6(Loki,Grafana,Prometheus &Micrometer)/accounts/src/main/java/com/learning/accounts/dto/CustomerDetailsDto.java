db524521ee16a2cf301baa754b3d0622c3270781
Your Name <you@example.com>
2026-01-30 19:08:57 +0530

    Monitoring and observability

---
+++ Bank_Microservices_v6(Loki,Grafana,Prometheus &Micrometer)/accounts/src/main/java/com/learning/accounts/dto/CustomerDetailsDto.java
@@ -0,0 +1,35 @@
+package com.learning.accounts.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+@Schema(
+        name="Customer",
+        description="Schema to hold Accounts, Cards, Loans & Customers information"
+)
+public class CustomerDetailsDto {
+    @NotEmpty(message = "Name is required")
+    @Size(min=3,max=30,message="Name should be between 3 and 30 characters")
+    @Schema(description="Name of the customer",example="Sriram Appan a")
+    private String name;
+    @NotEmpty(message="Email is required")
+    @Email(message="Email Address should be valid value")
+    @Schema(description="Email of the customer",example="Sriram@gmail.com")
+    private String email;
+    @Pattern(regexp = "$|[0-9]{10}",message="Mobile Number should be 10 digits value")
+    @Schema(description="Mobile Number of the customer",example="1234567890")
+    private String mobileNumber;
+    @Schema(description="Accounts of the customer")
+    private AccountsDto accountsDto;
+
+    @Schema(description="Cards of the customer")
+    private CardsDto cardsDto;
+
+    @Schema(description="Loans of the customer")
+    private LoansDto loansDto;
+}
---
+++ Bank_Microservices_v6(Loki,Grafana,Prometheus &Micrometer)/accounts/src/main/java/com/learning/accounts/service/client/LoansFallback.java
@@ -0,0 +1,15 @@
+package com.learning.accounts.service.client;
+
+import com.learning.accounts.dto.CardsDto;
+import com.learning.accounts.dto.LoansDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LoansFallback implements LoansFeignClient{
+
+    @Override
+    public ResponseEntity<LoansDto> fetchLoanDetails(String correlationId, String mobileNumber) {
+        return null;
+    }
+}
